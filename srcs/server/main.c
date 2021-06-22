60abb33fb9070463380d9b7864ef553824035761
Ugo DANIEL-THOMAS <ugo@air-de-ugo.lan>
2021-06-22 22:01:23 +0200

    added comments

--- srcs/server/main.c
+++ srcs/server/main.c
@@ -6,11 +6,26 @@
 /*   By: ugdaniel <ugdaniel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/21 13:59:41 by ugdaniel          #+#    #+#             */
-/*   Updated: 2021/06/22 16:22:21 by ugdaniel         ###   ########.fr       */
+/*   Updated: 2021/06/22 21:57:06 by ugdaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+/*
+ *	Example: message <a>
+ *
+ *	SIGUSR1 = 30, 00011110 in binary
+ *	SIGUSR2 = 31, 00011111 in binary
+ * 
+ *	a: 01100001 in binary
+ *	
+ *	unsigned char c is set to 00000000
+ *	
+ *	00011110 & 1 = 0
+ *	00011111 & 1 = 1
+ *	Basic bitwise shifting to the left until we reach 8 bits (for one character)
+*/
 
 void	signal_handler(int sig)
 {
